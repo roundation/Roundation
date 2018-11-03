0003eb626bce67323f991a2233e6408188426c66
Zheeeng <hi@zheeeng.me>
2018-11-03 14:30:00 +0800

    Not exporting Loadable anymore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { Router, Link, navigate, redirectTo } from '@reach/router'
-import * as Loadable from 'react-loadable'
 import Roundation from './react-binding/Roundation'
 
 export {
   Router,
   Link,
-  Loadable,
   navigate,
   redirectTo,
 }
