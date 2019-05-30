12e86fe20058d5feadc72ff3fda1208d33385973
Zheeeng <hi@zheeeng.me>
2019-05-30 10:37:08 +0800

    Clean quires supporting array type

--- src/utils/clean-object.ts
+++ src/utils/clean-object.ts
@@ -1,9 +1,14 @@
 import getKeys from './get-keys'
 
+function shouldDelete (value: any): boolean {
+  return value === null || value === undefined || value === ''
+    || (Array.isArray(value) && value.filter(v => !shouldDelete(v)).length === 0)
+}
+
 function cleanObject<O extends object> (o: O): object {
   const copy = { ...o }
   getKeys(copy).forEach(key => {
-    if (copy[key] === null || copy[key] === undefined || copy[key] === '') {
+    if (shouldDelete(copy[key])) {
       delete copy[key]
     }
   });
